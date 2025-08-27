f3e2a356480e955e547657afbee2f11421420194
Hayden Bleasel <hello@haydenbleasel.com>
2025-08-26 22:44:50 -0700

    Migrate examples

---
+++ packages/examples/src/loader.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Loader } from '@repo/elements/loader';
+
+const Example = () => {
+  return (
+    <div className="flex items-center justify-center p-8">
+      <Loader />
+    </div>
+  );
+};
+
+export default Example;
