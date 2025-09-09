5042ea5b3208acd3a95eb1b5be79fda0dfa9b355
Hayden Bleasel <hello@haydenbleasel.com>
2025-09-08 20:52:59 -0700

    Rebuild context component

--- apps/test/app/examples/context.tsx
+++ apps/test/app/examples/context.tsx
@@ -1,13 +1,45 @@
 "use client";
 
-import { Context } from "@repo/elements/context";
+import {
+  Context,
+  ContextCacheUsage,
+  ContextContent,
+  ContextContentBody,
+  ContextContentFooter,
+  ContextContentHeader,
+  ContextInputUsage,
+  ContextOutputUsage,
+  ContextReasoningUsage,
+  ContextTrigger,
+} from "@repo/elements/context";
 
-const Example = () => {
-  return (
-    <div className="flex items-center justify-center p-8">
-      <Context maxTokens={272_000} usedTokens={50_800} />
-    </div>
-  );
-};
+const Example = () => (
+  <div className="flex items-center justify-center p-8">
+    <Context
+      maxTokens={128_000}
+      modelId="openai:gpt-5"
+      usage={{
+        inputTokens: 32_000,
+        outputTokens: 8000,
+        totalTokens: 40_000,
+        cachedInputTokens: 0,
+        reasoningTokens: 0,
+      }}
+      usedTokens={40_000}
+    >
+      <ContextTrigger />
+      <ContextContent>
+        <ContextContentHeader />
+        <ContextContentBody>
+          <ContextInputUsage />
+          <ContextOutputUsage />
+          <ContextReasoningUsage />
+          <ContextCacheUsage />
+        </ContextContentBody>
+        <ContextContentFooter />
+      </ContextContent>
+    </Context>
+  </div>
+);
 
 export default Example;
