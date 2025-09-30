dff2b11d3d8f78d1838493f941bd7868e206bd3c
Hayden Bleasel <hello@haydenbleasel.com>
2025-09-30 15:00:13 +1000

    Update node.tsx

--- packages/elements/src/node.tsx
+++ packages/elements/src/node.tsx
@@ -21,7 +21,7 @@ export type NodeProps = ComponentProps<typeof Card> & {
 export const Node = ({ handles, className, ...props }: NodeProps) => (
   <Card
     className={cn(
-      "relative size-full h-auto w-sm gap-0 rounded-sm p-0",
+      "relative size-full h-auto w-sm gap-0 rounded-md p-0",
       className
     )}
     {...props}
@@ -36,7 +36,7 @@ export type NodeHeaderProps = ComponentProps<typeof CardHeader>;
 
 export const NodeHeader = ({ className, ...props }: NodeHeaderProps) => (
   <CardHeader
-    className={cn("gap-0.5 rounded-t-sm border-b bg-secondary p-3!", className)}
+    className={cn("gap-0.5 rounded-t-md border-b bg-secondary p-3!", className)}
     {...props}
   />
 );
@@ -65,7 +65,7 @@ export type NodeFooterProps = ComponentProps<typeof CardFooter>;
 
 export const NodeFooter = ({ className, ...props }: NodeFooterProps) => (
   <CardFooter
-    className={cn("rounded-b-sm border-t bg-secondary p-3!", className)}
+    className={cn("rounded-b-md border-t bg-secondary p-3!", className)}
     {...props}
   />
 );
