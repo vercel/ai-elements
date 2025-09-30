56e421768072646149e98e78194d197e6c834e1f
Hayden Bleasel <hello@haydenbleasel.com>
2025-09-30 14:56:28 +1000

    Update components

--- packages/elements/src/controls.tsx
+++ packages/elements/src/controls.tsx
@@ -1,16 +1,17 @@
-"use client";
+'use client';
 
-import { Controls as ControlsPrimitive } from "@xyflow/react";
-import type { ComponentProps } from "react";
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
+import { Controls as ControlsPrimitive } from '@xyflow/react';
+import type { ComponentProps } from 'react';
 
 export type ControlsProps = ComponentProps<typeof ControlsPrimitive>;
 
 export const Controls = ({ className, ...props }: ControlsProps) => (
   <ControlsPrimitive
     className={cn(
-      "overflow-hidden rounded-md border bg-card shadow-none!",
-      className
+      'rounded-md border gap-px bg-card p-1 overflow-hidden shadow-none!',
+      '[&>button]:rounded-md [&>button]:border-none',
+      className,
     )}
     {...props}
   />
