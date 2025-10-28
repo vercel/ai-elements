87879ad4817d4d90c2815df1fb764159f2233fb4
Hayden Bleasel <hello@haydenbleasel.com>
2025-10-27 21:03:44 -0700

    Misc fixes

--- packages/examples/src/web-preview.tsx
+++ packages/examples/src/web-preview.tsx
@@ -36,21 +36,6 @@ const exampleLogs = [
   },
 ];
 
-const code = [
-  {
-    language: "jsx",
-    filename: "MyComponent.jsx",
-    code: `function MyComponent(props) {
-  return (
-    <div>
-      <h1>Hello, {props.name}!</h1>
-      <p>This is an example React component.</p>
-    </div>
-  );
-}`,
-  },
-];
-
 const Example = () => {
   const [fullscreen, setFullscreen] = useState(false);
 
