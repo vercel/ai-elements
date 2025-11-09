80f8ffd84c9cb43785dace0d515e9d5c5d45f0a1
Hayden Bleasel <hello@haydenbleasel.com>
2025-11-08 18:14:14 -0800

    Fix search

--- apps/docs/components/navbar/custom-search.tsx
+++ apps/docs/components/navbar/custom-search.tsx
@@ -1,16 +1,21 @@
 import { Button } from "@repo/shadcn-ui/components/ui/button";
 import { Kbd } from "@repo/shadcn-ui/components/ui/kbd";
+import { useSearchContext } from "fumadocs-ui/contexts/search";
 
-export const CustomSearch = () => (
-  <Button
-    className="w-[153.86px] justify-between rounded-sm border-none pr-[7px] font-normal text-muted-foreground shadow-none ring-1 ring-border"
-    size="sm"
-    variant="outline"
-  >
-    <span>Search...</span>
-    <Kbd className="flex h-5 min-h-5 min-w-5 items-center gap-[2.2px] rounded-xs border-none bg-transparent px-1 py-0 font-normal font-sans text-muted-foreground text-xs ring-1 ring-border">
-      <span>⌘</span>
-      <span>K</span>
-    </Kbd>
-  </Button>
-);
+export const CustomSearch = () => {
+  const { setOpenSearch } = useSearchContext();
+  return (
+    <Button
+      className="w-[153.86px] justify-between rounded-sm border-none pr-[7px] font-normal text-muted-foreground shadow-none ring-1 ring-border"
+      onClick={() => setOpenSearch(true)}
+      size="sm"
+      variant="outline"
+    >
+      <span>Search...</span>
+      <Kbd className="flex h-5 min-h-5 min-w-5 items-center gap-[2.2px] rounded-xs border-none bg-transparent px-1 py-0 font-normal font-sans text-muted-foreground text-xs ring-1 ring-border">
+        <span>⌘</span>
+        <span>K</span>
+      </Kbd>
+    </Button>
+  );
+};
