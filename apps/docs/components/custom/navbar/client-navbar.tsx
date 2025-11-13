cdfbd50718bec45e67dd5d3de44e5d66c1c31342
Hayden Bleasel <hello@haydenbleasel.com>
2025-11-13 12:51:15 -0800

    Update client-navbar.tsx

--- apps/docs/components/custom/navbar/client-navbar.tsx
+++ apps/docs/components/custom/navbar/client-navbar.tsx
@@ -49,7 +49,7 @@ const LogoGithub = () => (
 );
 
 export const ClientNavbar = ({ pages }: { pages: NavPageItem[] }) => (
-  <div className="sticky top-0 flex items-center justify-between bg-background/80 py-2.5 backdrop-blur-sm">
+  <div className="sticky top-0 z-40 flex items-center justify-between bg-background py-2.5">
     <div className="flex select-none flex-row items-center">
       <div className="flex shrink-0 flex-row items-center gap-2">
         <HomeLinks />
